c7204d7a8f53cf144d70036a5369a18a3485983d
marekprog <marek.pierzchala.programista@gmail.com>
2019-06-11 12:14:41 +0200

    added balance table and js file

---
+++ public_html/time.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function setToday()
+{
+    document.getElementById("today").valueAsDate= new Date();
+}
+
+function showDiv() {
+   document.getElementById('dateRange').style.display = "block";
+}
